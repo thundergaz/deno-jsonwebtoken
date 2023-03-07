a56ac368b3b6d0208751c2d367d62864c5de0100
sturat <wqiosa@163.com>
2023-03-07 15:32:22 +0800

    Update mod.ts
    更改文件后辍

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
-export * as verify from './verify.ts';
-export * as sign from './sign.ts';
+export * as verify from './verify.js';
+export * as sign from './sign.js';
 export * as JsonWebTokenError from './lib/JsonWebTokenError.ts';
 export * as NotBeforeError from './lib/NotBeforeError.ts';
 export * as TokenExpiredError from './lib/TokenExpiredError.ts';
